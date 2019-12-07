0846bf71249f6f4181616eb2e3632e4582644582
kyoung90 <kenneth.young90@gmail.com>
2019-12-07 13:21:55 -0400

    Made stack on hover include grayscale and blur

--- src/features/Portfolio/Stack.js
+++ src/features/Portfolio/Stack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -6,10 +6,24 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fab);
 
 export const Stack = props => {
+  const [activeClass, setActiveClass] = useState("technology");
+
+  const onMouseEnterHandler = () => {
+    setActiveClass("technology-active");
+    props.setHover(true)
+  };
+
+  const onMouseLeaveHandler = () => {
+    setActiveClass("technology");
+    props.setHover(false)
+  };
+
   return (
     <div
-      className="d-flex mb-5 align-items-center technology"
+      className={`d-flex pb-5 align-items-center ${activeClass} ${props.hover && activeClass == "technology" ? "technology-blur" : ""}`}
       style={{ maxHeight: "60px", maxWidth: "60px" }}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
     >
       <figure>
         <img
@@ -17,7 +31,9 @@ export const Stack = props => {
           src={`./assets/logos/${props.tech}.svg`}
           alt=""
         />
-        <figcaption className="technology-name text-capitalize">{props.tech}</figcaption>
+        <figcaption className="technology-name text-capitalize">
+          {props.tech}
+        </figcaption>
       </figure>
     </div>
   );
--- src/features/Portfolio/StackContainer.js
+++ src/features/Portfolio/StackContainer.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "./Stack";
 
 export const StackContainer = props => {
+  const [hover, setHover] = useState(false)
+
   const generateStackIcons = () => {
-    return props.stack.map(tech => <Stack tech={tech} />);
+    return props.stack.map(tech => <Stack tech={tech} setHover={setHover} hover={hover}/>);
   };
+
+
+
   return (
     <div>
-      <div className="d-flex justify-content-around">{generateStackIcons()}</div>
+      <div className="d-flex justify-content-around stack-container">
+        {generateStackIcons()}
+      </div>
     </div>
   );
 };
