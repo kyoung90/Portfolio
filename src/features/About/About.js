7ae88443b1a2b8845987f6397176a18c29f4bb20
kyoung90 <kenneth.young90@gmail.com>
2019-12-07 17:14:38 -0400

    Added about section

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -17,6 +17,47 @@ export const About = () => {
           </div>
           <div className="divider-custom-line"></div>
         </div>
+
+        <div className="container pt-2">
+          <div className="row d-flex justify-content-around">
+            <div className="col-md-6 col-sm-8 d-flex flex-column align-items-center align-self-center">
+              <h3 className="text-secondary">
+                Get My Resume!
+              </h3>
+              <br/>
+              <a className="anchor-resume" href="/assets/kenneth_resume.pdf" target="_blank">
+                <img
+                  style={{ height: "250px", width: "250px" }}
+                  src="/assets/resume-icon.svg"
+                />
+              </a>
+            </div>
+            <div className="col-md-6 col-sm-8">
+              <h3 className="text-secondary">
+                Hi, I'm Kenneth. Nice to meet you.
+              </h3>
+              <br />
+              <p className="text-secondary heavy-text">
+                I began my coding journey when I was a senior in High School. I
+                started as a tutor in university and have teached code to High
+                School teachers and students both remotely and in the classroom.{" "}
+              </p>
+              <br />
+              <p className="text-secondary heavy-text">
+                I enjoy contributing to open source organizations and writing
+                tech blogs on Medium.{" "}
+              </p>
+              <br />
+              <p className="text-secondary heavy-text">
+                Take a look at what I'm working on recently on my{" "}
+                <a className="anchor-purple" href="https://github.com/kyoung90">
+                  github
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
