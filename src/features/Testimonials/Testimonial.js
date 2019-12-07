5f66916c2f68ed2d5433c8551c42d6a81e8b0475
kyoung90 <kenneth.young90@gmail.com>
2019-12-07 15:16:15 -0400

    Made testimonial image round and gave it a max width of 200px

--- src/features/Testimonials/Testimonial.js
+++ src/features/Testimonials/Testimonial.js
@@ -3,13 +3,13 @@ import React from "react";
 export const Testimonial = props => {
   return (
     <div className="testimonial-block">
-      <img className="avatar" src={props.image} />
+      <img className="testimonial-img" src={props.image} />
       <br />
       <br />
      
       <div className="container">
         <div class="row d-flex justify-content-center">
-          <div class="col-6">
+          <div class="col-lg-6 col-md-8 col-sm-12">
             <h5 className="quote" style={{ fontWeight:"normal" }}>{props.text}</h5>
             <br />
             <h4>{props.name}</h4>
