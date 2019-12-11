b88d67ce5e9b4d0f160fc69fe5190bf2c659d54f
kyoung90 <kenneth.young90@gmail.com>
2019-12-11 10:25:51 -0400

    Changed from class to className in all tags

--- src/features/Footer.js
+++ src/features/Footer.js
@@ -8,20 +8,20 @@ library.add(fab);
 export const Footer = () => {
   return (
     <div>
-      <footer class="footer text-center">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4 mb-5 mb-lg-0">
+      <footer className="footer text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 mb-5 mb-lg-0">
               <a href="/assets/kenneth_resume.pdf" target="_blank">
-                <h4 class="text-uppercase mb-4">Resume</h4>
+                <h4 className="text-uppercase mb-4">Resume</h4>
               </a>
             </div>
-            <div class="col-lg-4 mb-5 mb-lg-0">
-              <h4 class="text-uppercase mb-4">Follow Me At:</h4>
+            <div className="col-lg-4 mb-5 mb-lg-0">
+              <h4 className="text-uppercase mb-4">Follow Me At:</h4>
               <a
                 href="https://medium.com/@kenneth.young90"
                 target="_blank"
-                class="btn btn-outline-light btn-social mx-1"
+                className="btn btn-outline-light btn-social mx-1"
               >
                 <FontAwesomeIcon
                   style={{ fontSize: "1.4em" }}
@@ -31,7 +31,7 @@ export const Footer = () => {
               <a
                 href="https://www.linkedin.com/in/kenneth-young-castro-8a558b138/"
                 target="_blank"
-                class="btn btn-outline-light btn-social mx-1"
+                className="btn btn-outline-light btn-social mx-1"
               >
                 <FontAwesomeIcon
                   style={{ fontSize: "1.4em" }}
@@ -41,7 +41,7 @@ export const Footer = () => {
               <a
                 href="https://twitter.com/KennethYoungCas"
                 target="_blank"
-                class="btn btn-outline-light btn-social mx-1"
+                className="btn btn-outline-light btn-social mx-1"
               >
                 <FontAwesomeIcon
                   style={{ fontSize: "1.4em" }}
@@ -51,7 +51,7 @@ export const Footer = () => {
               <a
                 href="https://github.com/kyoung90"
                 target="_blank"
-                class="btn btn-outline-light btn-social mx-1"
+                className="btn btn-outline-light btn-social mx-1"
               >
                 <FontAwesomeIcon
                   style={{ fontSize: "1.4em" }}
@@ -59,20 +59,20 @@ export const Footer = () => {
                 />
               </a>
             </div>
-            <div class="col-lg-4 mb-5 mb-lg-0">
+            <div className="col-lg-4 mb-5 mb-lg-0">
               <a
                 href="mailto:kenneth.young90@gmail.com?Subject=Hey%20Kenneth!"
                 target="_top"
               >
-                <h4 class="text-uppercase mb-4">Contact me</h4>
+                <h4 className="text-uppercase mb-4">Contact me</h4>
               </a>
             </div>
           </div>
         </div>
       </footer>
 
-      <section class="copyright py-4 text-center text-white">
-        <div class="container">
+      <section className="copyright py-4 text-center text-white">
+        <div className="container">
           <small>Copyright &copy; Kenneth Young Castro 2019</small>
         </div>
       </section>
