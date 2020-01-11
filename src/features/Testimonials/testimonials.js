da842b5fc091b252241c091bf6c9b927a720cf23
kyoung90 <kenneth.young90@gmail.com>
2020-01-11 11:08:01 -0400

    Updated Burak image, added Dany testimonial

--- src/features/Testimonials/testimonials.js
+++ src/features/Testimonials/testimonials.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 export const testimonials = [
-  { name: "Joan Indiana", title: "Software Engineer at Vistaprint", text: "Kenneth has great ideas, knows how to organize and prioritize work and is a fabulous listener and collaborator! When I start my next company, he will be my first hire.", image: "https://media.licdn.com/dms/image/C4E03AQEoR07pOZoU6Q/profile-displayphoto-shrink_800_800/0?e=1580947200&v=beta&t=Yovg2XLwHFugTvA8aJlJZIHr0-cqcaL7V4QVuNSCWkw" }
-  ,{ name: "Burak Kocak", title: "Junior Software Developer at Young Consulting", text: "Kenneth is a great problem solver. He can come up with solutions in coding that many people can’t think of. He is not afraid of challenges. He doesn’t just write a code that works. Instead, He tries to write a code that works better than others.", image: "https://media.licdn.com/dms/image/C4E03AQHtK4JKM2nfBg/profile-displayphoto-shrink_800_800/0?e=1581552000&v=beta&t=O2BgiF8HhTj-XnduFx6oN8oWlQgZvG4B35m-6tOvDT4" }
+  {
+    name: "Joan Indiana",
+    title: "Software Engineer at Vistaprint",
+    text:
+      "Kenneth has great ideas, knows how to organize and prioritize work and is a fabulous listener and collaborator! When I start my next company, he will be my first hire.",
+    image:
+      "https://media.licdn.com/dms/image/C4E03AQEoR07pOZoU6Q/profile-displayphoto-shrink_800_800/0?e=1580947200&v=beta&t=Yovg2XLwHFugTvA8aJlJZIHr0-cqcaL7V4QVuNSCWkw"
+  },
+  {
+    name: "Burak Kocak",
+    title: "Junior Software Developer at Young Consulting",
+    text:
+      "Kenneth is a great problem solver. He can come up with solutions in coding that many people can’t think of. He is not afraid of challenges. He doesn’t just write a code that works. Instead, He tries to write a code that works better than others.",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGkGDAcUVP3fg/profile-displayphoto-shrink_800_800/0?e=1583971200&v=beta&t=ZHDIBrWiLscJ6nR3y33ToTEMZ7qa9yPzQxvdXWuBphc"
+  },
+  {
+    name: "Dany Villafuerte",
+    title: "Principal at Truenorth Corporation",
+    text:
+      "Kenneth is passionate about software coding. He likes challenges and, above all, he likes to learn. These qualities, along with his pragmatic vision of technology, allow him to become a valuable resource in any team he becomes part of.",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C5603AQFJhG6E-U_WBw/profile-displayphoto-shrink_800_800/0?e=1583971200&v=beta&t=PZ9zlPwHY5fGYmc_IxHn7OoBSQVYx5C_pPS4WsEjioo"
+  }
 ];
